795ae33311f50396ce50053d4a5fda14664f0fb9
digitribesthlm <patrik@makeablesthlm.se>
2024-05-14 00:24:50 +0200

    404

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+// pages/404.js
+
+export default function Custom404() {
+    return (
+        <div style={{ padding: "100px 20px", textAlign: "center", fontSize: "24px" }}>
+            <h1>404 - Page Not Found</h1>
+            <p>Oops! The page you are looking for has disappeared or never existed.</p>
+        </div>
+    );
+}
