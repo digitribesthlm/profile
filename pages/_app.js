0035c4421c3964ac975b2769d56070d092889ce7
digitribesthlm <patrik@makeablesthlm.se>
2024-05-16 20:43:20 +0200

    gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,10 @@
-import { useEffect } from 'react';
-import { AppProps } from 'next/app';
-import TagManager from 'react-gtm-module';
 import '@/styles/globals.css';
+import GTM from '../lib/gtm';
 
-function App({ Component, pageProps }: AppProps) {
-  // Google Tag Manager
-  useEffect(() => {
-    const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
-    if (GTM_ID) {
-      TagManager.initialize({ gtmId: GTM_ID });
-    } else {
-      console.error('GTM_ID is not defined');
-    }
-  }, []);
-
+function App({ Component, pageProps }) {
   return (
     <>
+      <GTM />
       <Component {...pageProps} />
     </>
   );
