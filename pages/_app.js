50b9a9caebb18385e031db801d21673360927bdb
digitribesthlm <patrik@makeablesthlm.se>
2024-05-16 20:30:49 +0200

    Added Google Tag Manager initialization using react-gtm-module in _app.js.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
+import { useEffect } from 'react';
+import { AppProps } from 'next/app';
+import TagManager from 'react-gtm-module';
 import '@/styles/globals.css';
-import GTM from '../lib/gtm';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
+  // Google Tag Manager
+  useEffect(() => {
+    const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+    if (GTM_ID) {
+      TagManager.initialize({ gtmId: GTM_ID });
+    } else {
+      console.error('GTM_ID is not defined');
+    }
+  }, []);
+
   return (
     <>
-      <GTM />
       <Component {...pageProps} />
     </>
   );
