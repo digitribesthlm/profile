99a128b62db23b826e63ea318a41ad4dadc9e654
digitribesthlm <patrik@makeablesthlm.se>
2024-05-16 17:22:27 +0200

    GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css';
+import GTM from '../lib/gtm';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GTM />
+      <Component {...pageProps} />
+    </>
+  );
 }
