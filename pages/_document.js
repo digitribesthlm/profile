243a3d572d7481ace12bac8aeede3245801e2378
digitribesthlm <patrik@makeablesthlm.se>
2024-05-17 10:09:30 +0200

    aider: Add Google Tag Manager script and noscript to pages/_document.js.
    # Aider chat conversation:
    
    USER: i need this to be in the header: <!-- Google Tag Manager -->
    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
    })(window,document,'script','dataLayer','GTM-WDPCNVN');</script>
    <!-- End Google Tag Manager --> and this after the body tag start <!-- Google Tag Manager (noscript) -->
    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WDPCNVN"
    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
    <!-- End Google Tag Manager (noscript) -->
    
    ASSISTANT: To implement the changes you've requested, I would need to modify the `_document.js` file to include the Google Tag Manager script in the header and the noscript after the body tag start. However, the `_document.js` file is currently read-only. If you would like to proceed with these changes, please add the `_document.js` file to the chat as a read-write file.
    
    USER: I added these *read-write* files: pages/_document.js
    
    ASSISTANT: Here are the diffs for the changes you requested to the `pages/_document.js` file:
    
    ```diff
    --- pages/_document.js
    +++ pages/_document.js
    @@ ... @@
     export default function Document() {
       return (
         <Html lang="en">
           <Head>
    -        {GTM_ID && (
    -          <script
    -            dangerouslySetInnerHTML={{ __html: gtmScriptContent }}
    -          />
    -        )}
    +        {/* Google Tag Manager */}
    +        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-WDPCNVN');` }} />
    +        {/* End Google Tag Manager */}
           </Head>
           <body>
    -        {GTM_ID && (
    -          <noscript dangerouslySetInnerHTML={{ __html: gtmNoScriptContent }} />
    -        )}
    +        {/* Google Tag Manager (noscript) */}
    +        <noscript>
    +          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WDPCNVN"
    +          height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe>
    +        </noscript>
    +        {/* End Google Tag Manager (noscript) */}
             <Main />
             <NextScript />
           </body>
         </Html>
       );
     }
    ```
    
    This diff removes the existing Google Tag Manager (GTM) code that was conditionally included based on the `GTM_ID` and replaces it with the GTM script and noscript iframe you provided. The GTM script is now directly included in the `<Head>` section, and the noscript iframe is included right after the opening `<body>` tag.

--- pages/_document.js
+++ pages/_document.js
@@ -5,16 +5,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {GTM_ID && (
-          <script
-            dangerouslySetInnerHTML={{ __html: gtmScriptContent }}
-          />
-        )}
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-WDPCNVN');` }} />
+        {/* End Google Tag Manager */}
       </Head>
       <body>
-        {GTM_ID && (
-          <noscript dangerouslySetInnerHTML={{ __html: gtmNoScriptContent }} />
-        )}
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WDPCNVN"
+          height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
