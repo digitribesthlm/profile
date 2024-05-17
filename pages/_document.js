a0ff73edcbb1c49ec6d1a172bb2c8e538b6a409a
digitribesthlm <patrik@makeablesthlm.se>
2024-05-17 10:09:29 +0200

    Refactored _document.js to import GTM_ID, gtmScriptContent, and gtmNoScriptContent for Google Tag Manager integration.

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-
-const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+import { GTM_ID, gtmScriptContent, gtmNoScriptContent } from '../lib/gtm';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
-        {/* Google Tag Manager (noscript) */}
+      <Head>
         {GTM_ID && (
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
+          <script
+            dangerouslySetInnerHTML={{ __html: gtmScriptContent }}
+          />
         )}
-        {/* End Google Tag Manager (noscript) */}
+      </Head>
+      <body>
+        {GTM_ID && (
+          <noscript dangerouslySetInnerHTML={{ __html: gtmNoScriptContent }} />
+        )}
         <Main />
         <NextScript />
       </body>
