978760f43e269161d71c54640766f05d1019134a
digitribesthlm <patrik@makeablesthlm.se>
2024-10-09 20:38:20 +0200

    chang

--- pages/index.js
+++ pages/index.js
@@ -80,7 +80,7 @@ export default function Home({ googleSiteVerification }) {
               rel="noopener noreferrer"
               className="btn btn-outline btn-sm lg:btn-md"
             >
-              Let's Talk 2024
+              Let's Talk
             </a>
             <a
               href="https://www.resume.se/affarer/byraer/pr-kollektivet-byter-namn-och-expanderar-blir-hybrid/"
